945074523d4f755d5365ea8d7f9e384c9db75af1
mamun-prodhan <mamunpr9@gmail.com>
2023-09-22 22:50:27 +0600

    form submitted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import ControlledForm from "./components/ControlledForm/ControlledForm";
 import SimpleForm from "./components/SimpleForm/SimpleForm";
 
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <SimpleForm></SimpleForm>
+      {/* <SimpleForm></SimpleForm> */}
+      <ControlledForm></ControlledForm>
     </>
   );
 }
---
+++ src/components/ControlledForm/ControlledForm.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+const ControlledForm = () => {
+  const [error, setError] = useState("");
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password.length < 6) {
+      setError("Password must be at least 4 characters");
+    } else {
+      console.log(name, email, password);
+      setError("");
+    }
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+    // const currentName = e.target.value;
+    // if (currentName.length < 10) {
+    //   setError("Name must be 10 characters");
+    // } else {
+    //   setName(e.target.value);
+    //   setError("");
+    // }
+  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    // const currentEmail = e.target.value;
+    // if (currentEmail.length < 15) {
+    //   setError("Email must be 15 characters");
+    // } else {
+    //   setEmail(e.target.value);
+    //   setError("");
+    // }
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleNameChange}
+          type="text"
+          name="name"
+          placeholder="Name"
+        />{" "}
+        <br />
+        <input
+          onChange={handleEmailChange}
+          type="email"
+          name="email"
+          placeholder="Email"
+        />{" "}
+        <br />
+        <input
+          onChange={handlePasswordChange}
+          type="password"
+          name="password"
+          placeholder="Password"
+        />{" "}
+        <br />
+        <input type="submit" value="Submit" />
+        {error && <p>{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default ControlledForm;
