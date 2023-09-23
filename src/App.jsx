355d7cde88811d7e04428bd84bd83d8e3549279f
mamun-prodhan <mamunpr9@gmail.com>
2023-09-23 14:05:22 +0600

    Reusable Form completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import "./App.css";
+import ReusableForm from "./components/ReusableForm/ReusableForm";
 import RefForm from "./components/refForm/refForm";
 // import ControlledForm from "./components/ControlledForm/ControlledForm";
 // import SimpleForm from "./components/SimpleForm/SimpleForm";
 
 function App() {
+  const handleSignUpSubmit = (data) => {
+    console.log("Sign Up data ", data);
+  };
+  const handleUpdateProfile = (data) => {
+    console.log("Update Profile Data ", data);
+  };
   return (
     <>
       <h1>Vite + React</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <ControlledForm></ControlledForm> */}
-      <RefForm></RefForm>
+      {/* <RefForm></RefForm> */}
+      <ReusableForm
+        formTitle={"Sign Up"}
+        submitBtnText={"Submit"}
+        handleSubmit={handleSignUpSubmit}
+      >
+        <h2>Sign Up</h2>
+        <p>Please sign up right Now</p>
+      </ReusableForm>
+      <ReusableForm
+        formTitle={"Update Profile"}
+        submitBtnText={"Update"}
+        handleSubmit={handleUpdateProfile}
+      >
+        <h2>Update Profile</h2>
+        <p>Please Keep your profile Updated</p>
+      </ReusableForm>
     </>
   );
 }
